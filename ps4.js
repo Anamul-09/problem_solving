788c260769d3f865b0590f7b15e7c42ad1f90ecb
Anamul-09 <mdanamulislam09@gmail.com>
2022-03-19 12:06:34 +0600

    first commit

---
+++ ps4.js
@@ -0,0 +1,8 @@
+
+// var leapyear = prompt("enter a year")
+// function leapyear(year){
+//     return (year % 100===0) ? (year % 400===0) : (year % 4===0); 
+// }
+
+// console.log(leapyear("leafyear"));
+
