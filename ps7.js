8d37d7dc8ae7cdbbee6795510cc31b5029e5b902
Anamul-09 <mdanamulislam09@gmail.com>
2022-03-31 13:14:08 +0600

    first commit

---
+++ ps7.js
@@ -0,0 +1,24 @@
+var marks =prompt("Enter your marks");
+
+if(marks>100 || marks<0)
+console.log("Invalid marks");
+
+else if(marks>=80)
+console.log("A+");
+
+else if(marks>=70)
+console.log("A");
+
+else if(marks>=60)
+console.log("A-");
+
+else if(marks>=50)
+console.log("B");
+
+else if(marks>=40)
+console.log("C");
+
+else if(marks<40)
+console.log("You have failed");
+
+
