22e0fb8cd82a6588f14a1b40eb534371cdd86f8c
Anamul-09 <mdanamulislam09@gmail.com>
2022-04-08 01:48:45 +0600

    second commit

---
+++ T operater.js
@@ -0,0 +1,40 @@
+
+
+//  var number = (prompt("Enter a number : "));
+
+
+
+// if(number>0){
+//     console.log("positive")
+// }
+// else if(number<0){
+//     console.log("negative")
+// }
+// else{
+//     console.log("Zero")
+// }
+
+
+
+
+
+// var result = number > 0 ? "positive": "negative";
+// console.log(result);
+
+
+
+
+
+// var result =number>0 ? console.log("positi0ve") : console.log("negative");
+
+
+
+// 3 conditions
+
+
+
+// var number = prompt("Enter a digit :");
+
+// var result = number > 0 ? "positive" : number < 0 ? "negative" : "Zero";
+
+// console.log(result);;
