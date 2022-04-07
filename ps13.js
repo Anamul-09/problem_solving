659e2b8102ad865830a0d334cd789fcccd97bea2
Anamul-09 <mdanamulislam09@gmail.com>
2022-04-08 02:40:52 +0600

    second commit

---
+++ ps13.js
@@ -0,0 +1,40 @@
+// function squire(num){
+ 
+//     var result = num*num;
+//     document.write(" squire = " + result + "<br>")
+// }
+
+// squire(6);
+// squire(5)
+
+
+
+function addition(x,y){
+    var result =x+y;
+    document.write("sum = " + result + "<br>")
+}
+
+
+function subdition(x,y){
+    var result =x/y;
+    document.write("Divide = " + result + "<br>")
+}
+
+
+function noaddition(x,y){
+    var result =x-y;
+    document.write("sum = " + result + "<br>")
+}
+
+
+function multiple(x,y){
+    var result =x*y;
+    document.write("sum = " + result + "<br>")
+}
+
+
+
+addition(79,55)
+subdition(500,50)
+noaddition(50,35)
+multiple(15,15)
