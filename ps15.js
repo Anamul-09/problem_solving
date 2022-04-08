28e64c05502e793e026001b71fb26bb07f50ecd6
Anamul-09 <mdanamulislam09@gmail.com>
2022-04-08 15:37:56 +0600

    second commit

---
+++ ps15.js
@@ -0,0 +1,58 @@
+//  var name1,name2,name3,name4,name5;
+
+//  name1="Anis";
+//  name2="Korim";
+//  name3="Rohim"
+//  name4="Jobbar";
+//  name5="Khaled";
+
+
+//  console.log(name5)
+
+
+                          //  Array  
+
+
+// var names = new Array();
+
+// names[0]="Anis";
+// names[1]="Korim";
+// names[2]="Jobbar";
+// names[3]="Salam";
+// names[4]="Salam";
+// names[5]="Kalam";
+
+// console.log(names[5]);
+// console.log(names);
+// console.log(names.length)
+
+
+                                 // push or pop
+
+
+
+// var names =["Anis","Korim","Jobbar","Salam","Salam","Kalam"];
+
+// names.push("Sokina","Karina");
+
+
+// console.log(names[2]);
+// console.log(names.length);
+// console.log(names)
+
+
+// names.pop();
+// console.log(names);
+
+
+
+                        // Concatinate
+
+
+
+    // var country1=["Bangladesh","Pakistan"];
+    // var country2=["Nepal","China"];
+
+    // var country =country1.concat(country2);
+
+    // console.log(country)
