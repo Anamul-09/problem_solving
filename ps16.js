e6e963275b612501b104dab466b86c89dd009f27
Anamul-09 <mdanamulislam09@gmail.com>
2022-04-08 17:39:00 +0600

    second commit

---
+++ ps16.js
@@ -0,0 +1,50 @@
+// var names=["Anam","Kamal","Jamal","Roshid","Banna","Rohim","Korim"];
+
+// console.log(names)
+
+
+// // add pop
+
+// names.pop();
+// console.log(names)
+
+
+// // add push
+
+// names.push("Jafor");
+// console.log(names)
+
+
+// // add shift
+
+// names.shift();
+// console.log(names);
+
+// // add unshift
+
+// names.unshift("Muntaqim");
+// console.log(names)
+
+
+                   //  add splice
+
+                //    var names= ["Anam","Kamal","Jamal","Rohim","Jobbar"];
+                //   console.log(names);
+
+                //    names.splice(3,1,"Salam","Sokina");
+                //    console.log(names)
+
+                // names.sort();
+                // console.log(names)
+
+                // names.reverse();
+                // console.log(names)
+
+
+                var numbers =[2,5,12,23,1,9,5,55,32];
+                numbers.sort(function(a,b){
+                    return a-b;
+                })
+
+                console.log(numbers)
+
