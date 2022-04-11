768e2caf1ad59b8754a757748f82974d119ed493
Anamul-09 <mdanamulislam09@gmail.com>
2022-04-11 14:38:20 +0600

    second commit

---
+++ ps20.js
@@ -0,0 +1,27 @@
+//  function addStyle(){
+
+//     var myVar =document.querySelector("#paraId");
+//     myVar.classList.add("para-style");
+    
+
+//  }
+
+//  function removeStyle(){
+//     var myVar =document.querySelector("#paraId");
+//     myVar.classList.remove("para-style");
+
+//  }
+
+
+function addStyle(){
+    var para =document.querySelector("#paraId");
+    para.classList.add("my-para");
+
+}
+
+
+function removeStyle(){
+    var para =document.querySelector("#paraId");
+    para.classList.remove("my-para");
+
+}
