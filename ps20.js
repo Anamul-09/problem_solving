492ce70136c6b4408986a7b5212bc3e976e79eb0
Anamul-09 <mdanamulislam09@gmail.com>
2022-04-11 17:01:50 +0600

    second commit

--- ps20.js
+++ ps20.js
@@ -1,27 +1,26 @@
-//  function addStyle(){
+  function addStyle(){
 
-//     var myVar =document.querySelector("#paraId");
-//     myVar.classList.add("para-style");
-    
+     var myVar =document.querySelector("#paraId");
+     myVar.classList.add("my-para");  
 
-//  }
+  }
 
-//  function removeStyle(){
-//     var myVar =document.querySelector("#paraId");
-//     myVar.classList.remove("para-style");
+  function removeStyle(){
+     var myVar =document.querySelector("#paraId");
+    myVar.classList.remove("my-para");
 
-//  }
-
-
-function addStyle(){
-    var para =document.querySelector("#paraId");
-    para.classList.add("my-para");
-
-}
+  }
 
 
-function removeStyle(){
-    var para =document.querySelector("#paraId");
-    para.classList.remove("my-para");
+// var para =document.querySelector("#paraId");
 
-}
+// function addStyle(){
+//     para.classList.add("my-para");
+
+// }
+
+
+// function removeStyle(){
+//     para.classList.remove("my-para");
+
+// }
---
+++ ps21.js
@@ -0,0 +1,25 @@
+
+// var len = document.querySelectorAll(".mybtn").length
+
+// for(var i=0 ; i<len; i++){
+
+//     var btn1=document.querySelectorAll(".mybtn")[i].addEventListener("click", function(){
+
+//         var text =this.innerHTML
+//         document.querySelector("h1").innerHTML=text + " is clicked"
+    
+//     });
+    
+// }
+
+
+var len =document.querySelectorAll(".mybtn").length;
+
+for(var i=0 ; i<len ; i++){
+    var button =document.querySelectorAll(".mybtn")[i].addEventListener("click", function(){
+
+        var text =this.innerHTML;
+        document.querySelector("h1").innerHTML=text + "in clicked";
+    
+    })
+}
