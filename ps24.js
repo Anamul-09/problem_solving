a4d7f592799fde96a4059b44b1513af78707d71a
Anamul-09 <mdanamulislam09@gmail.com>
2022-04-13 01:45:25 +0600

    second commit

---
+++ ps24.js
@@ -0,0 +1,43 @@
+
+
+ for(var i=0 ; i<=3 ; i++){
+    document.querySelectorAll(".mybtn")[i].addEventListener("click",function(){
+        var text=this.innerHTML;
+      
+           console.log(text);
+
+           audioPlay(text);
+           playAnimation(text); 
+
+       
+           });
+       }      
+       
+       function audioPlay(text){
+           switch (text) {
+               case text:
+                   var audio=new Audio("music/1.mp3");
+                   audio.play();
+                 
+                   break;
+     
+               case text:
+                   var audio2=new Audio("music/2.mp3");
+                   audio2.play();
+                 
+                   break;
+     
+               case text:
+               var audio3=new Audio("music/3.mp3");
+                   audio3.play();
+                 
+                  break;
+           }
+       }
+
+
+function playAnimation(text){
+    var myVar =document.querySelector(".mybtn");
+    myVar.classList.add("anim")
+
+ }
